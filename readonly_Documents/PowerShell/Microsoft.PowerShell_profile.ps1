e056449e4dada9a53cfd734fd4476145bec7a07b
jinjiwu <jinji_wu@qq.com>
2026-01-16 04:28:58 +0800

    refactor(powerShell): 优化 PowerShell 配置文件内容
    - 移除多余的注释和冗余代码
    - 重新排列命令以提高可读性

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,13 @@
-Invoke-Expression (&starship init powershell)
 starship init powershell | Out-String | Invoke-Expression
-#f45873b3-b655-43a6-b217-97c00aa0db58 PowerToys CommandNotFound module
-
-Import-Module -Name Microsoft.WinGet.CommandNotFound
-#f45873b3-b655-43a6-b217-97c00aa0db58
-
-# Invoke-Expression (& { (zoxide init powershell | Out-String) })
 zoxide init powershell --cmd cd | Out-String | Invoke-Expression
+# uv shell completion
+(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
+(& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
+# eza
+function ls { eza --icons --git $args }
+function ll { eza -l --icons --git --time-style relative $args }
 
 # replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# uv 
-(& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
-(& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
+Import-Module -Name Microsoft.WinGet.CommandNotFound
