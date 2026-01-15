f3160f1949c62b8c083447259eb5f373c5040053
jinjiwu <jinji_wu@qq.com>
2026-01-16 06:55:43 +0800

    refactor(tmux): 优化配置文件注释和结构
    - tmux: 添加配置段落注释以提高可读性
    - xonsh: 添加插件和别名注释以增强可理解性
    - zsh: 重新组织插件和函数注释以改善结构
    - PowerShell: 增加函数注释以明确功能

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,26 @@
+# [init] tools initialization
 starship init powershell | Out-String | Invoke-Expression
 zoxide init powershell --cmd cd | Out-String | Invoke-Expression
-# uv shell completion
+# [init.psfzf] replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+# [init.uv.completion]
 (& uv generate-shell-completion powershell) | Out-String | Invoke-Expression
 (& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
-# eza
+
+# [winget]
+Import-Module -Name Microsoft.WinGet.CommandNotFound
+
+# [functions]
+# [functions.eza]
 function ls { eza --icons --git $args }
 function ll { eza -l --icons --git --time-style relative $args }
-
-# replace 'Ctrl+t' and 'Ctrl+r' with your preferred bindings:
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
-
-Import-Module -Name Microsoft.WinGet.CommandNotFound
+# [functions.yazi] `q` quit to current directory, `Q` quit to start directory
+function y {
+    $tmp = (New-TemporaryFile).FullName
+    yazi.exe $args --cwd-file="$tmp"
+    $cwd = Get-Content -Path $tmp -Encoding UTF8
+    if ($cwd -ne $PWD.Path -and (Test-Path -LiteralPath $cwd -PathType Container)) {
+        Set-Location -LiteralPath (Resolve-Path -LiteralPath $cwd).Path
+    }
+    Remove-Item -Path $tmp
+}
