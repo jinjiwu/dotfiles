509817b04b2521c8c902971dd643822796f9870c
jinjiwu <jinji_wu@qq.com>
2026-04-08 01:49:37 +0800

    feat(script): 更新apt安装脚本，添加zsh插件和zellij安装步骤

--- script/apt-install.sh
+++ script/apt-install.sh
@@ -4,8 +4,22 @@ apt update && apt install -y --no-install-recommends \
     wget \
     tmux \
     fzf \
+    zoxide \
     rsync \
-    zsh
+    chezmoi \
+    zsh \
+    zsh-autosuggestions \
+    zsh-syntax-highlighting 
+
+chsh -s $(which zsh)
+
+# mihomo, default config location: /etc/mihomo/config.yaml or ~/.config/mihomo/config.yaml
+version=v1.19.22
+wget https://github.com/MetaCubeX/mihomo/releases/download/{version}/mihomo-linux-amd64-v3-{version}.deb && \
+    dpkg -i mihomo-linux-amd64-v3-{version}.deb
+
+# chezmoi, default config location: ~/.local/share/chezmoi
+chezmoi init https://github.com/jinjiwu/dotfiles.git
 
 curl -sS https://starship.rs/install.sh | sh -s -- --yes 
 
@@ -13,6 +27,12 @@ curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh 
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
 
-# mihomo, default config location: /etc/mihomo/config.yaml or ~/.config/mihomo/config.yaml
-wget https://github.com/MetaCubeX/mihomo/releases/download/v1.19.15/mihomo-linux-amd64-v3-v1.19.15.deb && \
-    dpkg -i mihomo-linux-amd64-v3-v1.19.15.deb
+# [zellij.ubuntu>23.04]
+# apt install -y zellij
+# [zellij.old_system]
+wget https://github.com/zellij-org/zellij/releases/latest/download/zellij-x86_64-unknown-linux-musl.tar.gz
+# 解压
+tar -xvf zellij-x86_64-unknown-linux-musl.tar.gz
+# 赋予执行权限并移动到系统路径
+chmod +x zellij
+mv zellij .local/bin/
